681cb2d300043be2394ded0cbce14be1bde18d85
Synogun <neto.antoniorrvv@gmail.com>
2025-07-28 23:22:50 -0300

    chore: add way to update packages and update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import stylistic from '@stylistic/eslint-plugin'
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
     {
@@ -25,7 +25,7 @@ export default tseslint.config([
             'stylistic': stylistic
         },
     },
-        {
+    {
         languageOptions: {
             parserOptions: {
                 projectService: true,
@@ -35,54 +35,113 @@ export default tseslint.config([
     },
     {
         rules: {
-            'stylistic/array-bracket-newline': ["error", "consistent"],
-            'stylistic/array-bracket-spacing': ["error", "never"],
-            'stylistic/array-element-newline': ["error", "consistent"],
-            'stylistic/arrow-parens': [2, "as-needed", { "requireForBlockBody": true }],
-            'stylistic/arrow-spacing': "error",
+            // https://eslint.style/rules/array-bracket-newline
+            'stylistic/array-bracket-newline': ['error', 'consistent'],
+            // https://eslint.style/rules/array-bracket-spacing
+            'stylistic/array-bracket-spacing': ['error', 'never'],
+            // https://eslint.style/rules/array-element-newline
+            'stylistic/array-element-newline': ['error', 'consistent'],
+            // https://eslint.style/rules/arrow-parens
+            'stylistic/arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
+            // https://eslint.style/rules/arrow-spacing
+            'stylistic/arrow-spacing': 'error',
+            // https://eslint.style/rules/block-spacing
             'stylistic/block-spacing': 'error',
-            'stylistic/brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
-            'stylistic/comma-dangle': ["error", "always-multiline"],
-            'stylistic/comma-spacing': ["error", { "before": false, "after": true }],
-            'stylistic/comma-style': ["error", "last"],
-            'stylistic/curly-newline': ["error", { "consistent": true }],
-            'stylistic/dot-location': ["error", "property"],
-            'stylistic/eol-last': ["error", "always"],
-            'stylistic/function-call-spacing': ["error", "never"],
-            'stylistic/function-call-argument-newline': ["error", "consistent"],
-            'stylistic/function-paren-newline': ["error", "consistent"],
+            // https://eslint.style/rules/brace-style
+            'stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+            // https://eslint.style/rules/comma-dangle
+            'stylistic/comma-dangle': ['error', {
+                arrays: 'only-multiline',
+                objects: 'only-multiline',
+                imports: 'never',
+                exports: 'never',
+                functions: 'only-multiline',
+                importAttributes: 'only-multiline',
+                dynamicImports: 'ignore',
+                enums: 'only-multiline',
+                generics: 'only-multiline',
+                tuples: 'only-multiline',
+            }],
+            // https://eslint.style/rules/comma-spacing
+            'stylistic/comma-spacing': ['error', { before: false, after: true }],
+            // https://eslint.style/rules/comma-style
+            'stylistic/comma-style': ['error', 'last'],
+            // https://eslint.style/rules/curly-newline
+            'stylistic/curly-newline': ['error', { consistent: true }],
+            // https://eslint.style/rules/dot-location
+            'stylistic/dot-location': ['error', 'property'],
+            // https://eslint.style/rules/eol-last
+            'stylistic/eol-last': ['error', 'always'],
+            // https://eslint.style/rules/function-call-spacing
+            'stylistic/function-call-spacing': ['error', 'never'],
+            // https://eslint.style/rules/function-call-argument-newline
+            'stylistic/function-call-argument-newline': ['error', 'consistent'],
+            // https://eslint.style/rules/function-paren-newline
+            'stylistic/function-paren-newline': ['error', 'consistent'],
+            // https://eslint.style/rules/indent
             'stylistic/indent': ['error', 4],
+            // https://eslint.style/rules/indent-binary-ops
             'stylistic/indent-binary-ops': ['error', 4],
+            // https://eslint.style/rules/keyword-spacing
             'stylistic/keyword-spacing': ['error'],
-            'stylistic/member-delimiter-style': "error",
-            'stylistic/multiline-ternary': ["error", "always-multiline"],
-            'stylistic/new-parens': ["error", "never"],
-            'stylistic/no-extra-parens': ["error", "all", { "nestedBinaryExpressions": false }],
+            // https://eslint.style/rules/member-delimiter-style
+            'stylistic/member-delimiter-style': 'error',
+            // https://eslint.style/rules/multiline-ternary
+            'stylistic/multiline-ternary': ['error', 'always-multiline'],
+            // https://eslint.style/rules/new-parens
+            'stylistic/new-parens': ['error', 'never'],
+            // https://eslint.style/rules/no-extra-parens
+            'stylistic/no-extra-parens': ['error', 'all', { nestedBinaryExpressions: false }],
+            // https://eslint.style/rules/no-extra-semi
             'stylistic/no-extra-semi': 'error',
+            // https://eslint.style/rules/no-floating-decimal
             'stylistic/no-floating-decimal': 'error',
-            'stylistic/no-mixed-operators': "error",
-            'stylistic/no-mixed-spaces-and-tabs': "error",
-            'stylistic/no-multi-spaces': "error",
-            'stylistic/no-trailing-spaces': ['error', { 'skipBlankLines': true }],
-            'stylistic/no-whitespace-before-property': "error",
-            'stylistic/object-curly-newline': ["error", { "consistent": true }],
+            // https://eslint.style/rules/no-mixed-operators
+            'stylistic/no-mixed-operators': 'error',
+            // https://eslint.style/rules/no-mixed-spaces-and-tabs
+            'stylistic/no-mixed-spaces-and-tabs': 'error',
+            // https://eslint.style/rules/no-multi-spaces
+            'stylistic/no-multi-spaces': 'error',
+            // https://eslint.style/rules/no-trailing-spaces
+            'stylistic/no-trailing-spaces': ['error', { skipBlankLines: true }],
+            // https://eslint.style/rules/no-whitespace-before-property
+            'stylistic/no-whitespace-before-property': 'error',
+            // https://eslint.style/rules/object-curly-newline
+            'stylistic/object-curly-newline': ['error', { consistent: true }],
+            // https://eslint.style/rules/object-curly-spacing
             'stylistic/object-curly-spacing': ['error', 'always'],
-            'stylistic/operator-linebreak': ["error", "after", { "overrides": { "?": "before", ":": "before" } }],
-            'stylistic/quote-props': ["error", "consistent-as-needed"],
+            // https://eslint.style/rules/operator-linebreak
+            'stylistic/operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+            // https://eslint.style/rules/quote-props
+            'stylistic/quote-props': ['error', 'consistent-as-needed'],
+            // https://eslint.style/rules/quotes
             'stylistic/quotes': ['error', 'single'],
-            'stylistic/rest-spread-spacing': ["error", "never"],
+            // https://eslint.style/rules/rest-spread-spacing
+            'stylistic/rest-spread-spacing': ['error', 'never'],
+            // https://eslint.style/rules/semi
             'stylistic/semi': 'error',
-            'stylistic/semi-spacing': "error",
-            'stylistic/semi-style': ["error", "last"],
-            'stylistic/space-before-blocks': "error",
-            'stylistic/space-before-function-paren': ['error', { 'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always' }],
-            'stylistic/space-in-parens': ["error", "never"],
+            // https://eslint.style/rules/semi-spacing
+            'stylistic/semi-spacing': 'error',
+            // https://eslint.style/rules/semi-style
+            'stylistic/semi-style': ['error', 'last'],
+            // https://eslint.style/rules/space-before-blocks
+            'stylistic/space-before-blocks': 'error',
+            // https://eslint.style/rules/space-before-function-paren
+            'stylistic/space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
+            // https://eslint.style/rules/space-in-parens
+            'stylistic/space-in-parens': ['error', 'never'],
+            // https://eslint.style/rules/space-unary-ops
             'stylistic/space-unary-ops': 'error',
+            // https://eslint.style/rules/switch-colon-spacing
             'stylistic/switch-colon-spacing': 'error',
+            // https://eslint.style/rules/template-curly-spacing
             'stylistic/template-curly-spacing': ['error', 'never'],
-            'stylistic/type-annotation-spacing': ['error', { 'before': false, 'after': true }],
-            'stylistic/type-generic-spacing': ["error"],
-            'stylistic/type-named-tuple-spacing': ["error"],
+            // https://eslint.style/rules/type-annotation-spacing
+            'stylistic/type-annotation-spacing': ['error', { before: false, after: true }],
+            // https://eslint.style/rules/type-generic-spacing
+            'stylistic/type-generic-spacing': ['error'],
+            // https://eslint.style/rules/type-named-tuple-spacing
+            'stylistic/type-named-tuple-spacing': ['error'],
         },
     },
     eslint.configs.recommended,
