ddfa5a077be2579f379e28d52045ccfb7274dbef
Synogun <neto.antoniorrvv@gmail.com>
2026-02-21 00:42:58 -0300

    feat: implement parsedError class

---
+++ src/config/parsedError.ts
@@ -0,0 +1,29 @@
+type Jsonable =
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | readonly Jsonable[]
+    | { readonly [key: string]: Jsonable }
+    | { toJSON(): Jsonable };
+
+export class ParsedError extends Error {
+    public readonly context?: Jsonable;
+
+    constructor(
+        message: string,
+        options: { cause?: Error; context?: Jsonable } = {}
+    ) {
+        const { cause, context } = options;
+        super(message, { cause });
+        this.context = context;
+    }
+}
+
+export function parseError(error: unknown, context?: Jsonable): ParsedError {
+    if (error instanceof Error) {
+        return new ParsedError(error.message, { cause: error, context });
+    }
+    return new ParsedError(String(error), { context });
+}
